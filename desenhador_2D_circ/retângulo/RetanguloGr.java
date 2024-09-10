e33cfbe66638f759b40af95f0d09b67265fcd7e2
Manoela Macchion Martedi <ra00331711@pucsp.edu.br>
2024-09-10 10:48:12 -0300

    added-retangulo

---
+++ desenhador_2D_circ/retângulo/RetanguloGr.java
@@ -0,0 +1,24 @@
+package retângulo;
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * Escreva uma descrição da classe RetanguloGr aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class RetanguloGr extends Retangulo {
+    // ATRIBUTOS -- RETÂNGULO GRÁFICO
+    Color corRetangulo = Color.BLACK;           // Cor padrão
+    String nomeRetangulo = "";                  // Nome padrão
+    //Color corNomeRetangulo  = Color.BLACK;    // Cor do nome padrão
+    int espRetangulo = 1;                       // Espessura padrão
+    
+    // CONSTRUTORES (to-do)
+    // ...
+    
+    // SETTERS E GETTERS (to-do)
+    
+    // OUTROS MÉTODOS (to-do)
+}
